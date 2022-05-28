08e244f9a33ca8cefb6572366b24fda945944e7c
Saimon Kat <saimon.kat23@gmail.com>
2022-05-28 12:52:49 +0300

    Service page fixes

--- src/js/pages/service.js
+++ src/js/pages/service.js
@@ -1,9 +1,24 @@
 export default function() {
     const servicePrice = document.querySelector('.service-price');
     const serviceWrapper = document.querySelector('.service-wrapper');
-    const isMobile = window.matchMedia('(max-width: 1199px)').matches;
+    const pageSidebar = document.querySelector('.page-sidebar');
+    let serviceMobile = false;
 
-    if (servicePrice && serviceWrapper && isMobile) {
-        serviceWrapper.appendChild(servicePrice);
+    if (servicePrice) {
+        const winResize = () => {
+            const isMobile = window.matchMedia('(max-width: 1199px)').matches;
+
+            if (isMobile && !serviceMobile) {
+                serviceWrapper.appendChild(servicePrice);
+                serviceMobile = true;
+            } else
+            if (!isMobile && serviceMobile) {
+                pageSidebar.appendChild(servicePrice);
+                serviceMobile = false;
+            }
+        }
+
+        winResize();
+        window.addEventListener('resize', winResize);
     }
 }
